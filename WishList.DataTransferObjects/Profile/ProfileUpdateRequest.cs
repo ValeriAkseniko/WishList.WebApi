23ed4c086fc9dd14a66ce0fc48958cc862fc9c8b
ValerI Akseniko <valeri.akseniko@yandex.ru>
2021-11-18 15:37:09 +0300

    create user service

---
+++ WishList.DataTransferObjects/Profile/ProfileUpdateRequest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace WishList.DataTransferObjects.Profile
+{
+    public class ProfileUpdateRequest
+    {
+        [Required]
+        public string Nickname { get; set; }
+        public int Gender { get; set; }
+        public DateTime Birthday { get; set; }
+    }
+}
